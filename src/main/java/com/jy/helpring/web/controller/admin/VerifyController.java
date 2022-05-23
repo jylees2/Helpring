ca31d4898bcd1b6fe52c14fa16680c7a770b6da1
jylees2 <jyleedev@gmail.com>
2022-05-23 23:50:35 +0900

    강의 결제 API Iamport api 추가 & 서버 결제 검증

---
+++ src/main/java/com/jy/helpring/web/controller/admin/VerifyController.java
@@ -0,0 +1,38 @@
+package com.jy.helpring.web.controller.admin;
+
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/verifyIamport")
+public class VerifyController {
+
+    /** Iamport 결제 검증 컨트롤러 **/
+    private final IamportClient iamportClient;
+
+    // 생성자를 통해 REST API 와 REST API secret 입력
+    public VerifyController(){
+        this.iamportClient = new IamportClient("4556500950256242",
+                "f98fe01fd3806dd7443fd41074fc28edd1fae5244061228e33fc899e669ce106faece3f995cd6bf1");
+    }
+
+    /** 프론트에서 받은 PG사 결괏값을 통해 아임포트 토큰 발행 **/
+    @PostMapping("/{imp_uid}")
+    public IamportResponse<Payment> paymentByImpUid(@PathVariable String imp_uid) throws IamportResponseException, IOException{
+        log.info("paymentByImpUid 진입");
+        return iamportClient.paymentByImpUid(imp_uid);
+    }
+
+}
