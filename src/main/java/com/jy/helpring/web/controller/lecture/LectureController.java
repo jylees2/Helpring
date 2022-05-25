68d54a901c279a4eed46934f8b1e48c08d206870
jylees2 <jyleedev@gmail.com>
2022-05-25 23:49:26 +0900

    비로그인 유저 처리

--- src/main/java/com/jy/helpring/web/controller/lecture/LectureController.java
+++ src/main/java/com/jy/helpring/web/controller/lecture/LectureController.java
@@ -7,15 +7,14 @@ import com.jy.helpring.service.course.CourseService;
 import com.jy.helpring.service.lecture.LectureService;
 import com.jy.helpring.service.lecture.MyLectureService;
 import com.jy.helpring.service.review.ReviewService;
-import com.jy.helpring.web.dto.category.CategoryDto;
 import com.jy.helpring.web.dto.category.LectureCategoryDto;
 import com.jy.helpring.web.dto.course.CourseDto;
 import com.jy.helpring.web.dto.lecture.LectureDto;
+import com.jy.helpring.web.dto.mylecture.MyLectureDto;
 import com.jy.helpring.web.dto.review.ReviewDto;
 import com.jy.helpring.web.vo.PageVo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -44,9 +43,11 @@ public class LectureController {
                                  Pageable pageable,
                                  @AuthenticationPrincipal UserAdapter user,
                                  Model model){
+
         /* 강의 카테고리 리스트 반환 */
         List<LectureCategoryDto.ResponseDto> categoryList = categoryService.findLectureList();
         model.addAttribute("categoryList", categoryList);
+
 
         /** ========== 페이징 처리 ========== **/
         pageNo = (pageNo == 0) ? 0 : (pageNo - 1);
@@ -71,9 +72,13 @@ public class LectureController {
                           @AuthenticationPrincipal UserAdapter user,
                           Model model){
 
-        /* 강의 카테고리 리스트 반환 */
+        /* 강의 카테고리 리스트, 현재 카테고리 반환 */
         List<LectureCategoryDto.ResponseDto> categoryList = categoryService.findLectureList();
         model.addAttribute("categoryList", categoryList);
+        model.addAttribute("category_name", category_name);
+
+        String lectureViewName = categoryService.getLectureViewName(category_name);
+        model.addAttribute("category_viewName", lectureViewName);
 
         /** ========== 페이징 처리 ========== **/
         pageNo = (pageNo == 0) ? 0 : (pageNo - 1);
@@ -90,76 +95,71 @@ public class LectureController {
     }
 
     /** 강의 상세 조회 **/
-    /* /lecture/{lecture_id} */
-    @GetMapping("/{lecture_id}")
-    public String readByLectureId(@PathVariable Long lecture_id,
+    /* /lecture/{category_name}/{lecture_id} */
+    @GetMapping("/{category_name}/{lecture_id}")
+    public String readByLectureId(@PathVariable("category_name") String category_name,
+                                  @PathVariable("lecture_id") Long lecture_id,
                                   @AuthenticationPrincipal UserAdapter user,
                                   Model model){
 
-        Long member_id = user.getMemberDto().getId();
+        /** 구매한 강의인지 확인 lectureCheck, 구매한 강의라면 true, 구매하지 않은 강의라면 false **/
+        /** 리뷰한 강의인지 확인 reviewCheck **/
 
-        /** 강의, 리뷰 리스트 반환 **/
+        boolean lectureCheck;
+        boolean reviewCheck;
+
+        if(user != null){
+            // 로그인한 사용자라면
+            Long member_id = user.getMemberDto().getId();
+            model.addAttribute("member_id", member_id);
+
+            lectureCheck = lectureService.myLectureCheck(member_id, lecture_id);
+            reviewCheck = reviewService.reviewCheck(member_id, lecture_id);
+
+        } else {
+            // 로그인하지 않은 사용자도 false
+            lectureCheck = false;
+            reviewCheck = false;
+        }
+
+        model.addAttribute("lectureCheck", lectureCheck);
+        model.addAttribute("reviewCheck", reviewCheck);
+
+        /* 강의, 리뷰 목록 반환 */
         LectureDto.ResponseDto lecture = lectureService.getById(lecture_id);
         List<ReviewDto.ResponseDto> reviewList = reviewService.findAllByLecture(lecture_id);
+        List<CourseDto.ResponseDto> courseList = courseService.getAllById(lecture_id);
 
         model.addAttribute("lecture", lecture);
         model.addAttribute("reviewList", reviewList);
+        model.addAttribute("courseList", courseList);
 
-        /** 구매한 강의인지 확인 myLectureCheck **/
-        boolean lectureCheck;
-        if(user == null){
-            lectureCheck = false;
-        } else {
-            lectureCheck = lectureService.myLectureCheck(member_id, lecture_id);
-        }
-        model.addAttribute("lectureCheck", lectureCheck);
-
-        /** 리뷰한 강의인지 확인 myReviewCheck **/
-        boolean reviewCheck;
-        if(user == null){
-            reviewCheck = false;
-        } else {
-            reviewCheck = reviewService.reviewCheck(member_id, lecture_id);
-        }
-        model.addAttribute("reviewCheck", reviewCheck);
+        /* 강의 카테고리 리스트, 현재 카테고리 반환 */
+        List<LectureCategoryDto.ResponseDto> categoryList = categoryService.findLectureList();
+        model.addAttribute("categoryList", categoryList);
+        model.addAttribute("category_name", category_name);
 
         return "lecture/lecture-read";
     }
 
-    /** 강의 리스트 상세 조회 (강의 수강 화면) **/
+    /** 강의 리스트(영상) 상세 조회 (강의 수강 화면) **/
 
     /* /lecture/course/{course_id} */
     @GetMapping("/course/{course_id}")
     public String readByCourseId(@PathVariable Long course_id,
                                  Model model){
 
-        /** 수강할 강의 정보 **/
+        /* 수강할 강의 수업 정보 */
         CourseDto.ResponseDto course = courseService.getById(course_id);
 
-        /** 강의 리스트 반환 **/
+        /** 해당 강의의 수업 목록 반환 **/
         Long lecture_id = course.getLecture_id();
         List<CourseDto.ResponseDto> courseList = courseService.getAllById(lecture_id);
 
         model.addAttribute("course", course);
         model.addAttribute("courseList", courseList);
 
-        return "lecture/course";
-    }
-
-    /** 내가 수강 중인 강의 조회 **/
-    /* /lecture/mylecture */
-    @GetMapping("/myLecture")
-    public String myLecture(@AuthenticationPrincipal UserAdapter user,
-                            Model model){
-
-        Long member_id = user.getMemberDto().getId();
-
-        List<CourseDto.ResponseDto> courseList = courseService.getAllById(member_id);
-
-        model.addAttribute("courseList", courseList);
-        model.addAttribute("user", user);
-
-        return "lecture/myLecture";
+        return "lecture/lecture-course";
     }
 
     /** 강의 결제 성공 후 수강 권한 획득 **/
@@ -176,6 +176,6 @@ public class LectureController {
         cartService.deleteAll(member_id);
 
         /* lecture/myClass 로 리다이렉트 */
-        return "redirect:/lecture/lecture";
+        return "redirect:/myLecture";
     }
 }
--- src/main/java/com/jy/helpring/web/controller/post/PostController.java
+++ src/main/java/com/jy/helpring/web/controller/post/PostController.java
@@ -4,7 +4,7 @@ import com.jy.helpring.config.auth.UserAdapter;
 import com.jy.helpring.service.category.CategoryService;
 import com.jy.helpring.service.comment.CommentService;
 import com.jy.helpring.service.post.PostService;
-import com.jy.helpring.web.dto.category.CategoryDto;
+import com.jy.helpring.web.dto.category.PostCategoryDto;
 import com.jy.helpring.web.dto.comment.CommentDto;
 import com.jy.helpring.web.dto.post.PostDto;
 import com.jy.helpring.web.vo.PageVo;
@@ -15,16 +15,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/community")
+@RequestMapping("/community/post")
 @Slf4j
 public class PostController {
 
@@ -33,9 +32,27 @@ public class PostController {
     
     private final CategoryService categoryService;
 
+    /** 글 저장 **/
+    @PostMapping("/save")
+    public String save(@Validated @ModelAttribute PostDto.RequestDto postDto,
+                     @AuthenticationPrincipal UserAdapter user) throws IOException {
+
+        log.info("/community/post/save 진입");
+        Long memberId = user.getMember().getId();
+
+        // 글 저장
+        Long saveId = postService.save(postDto, memberId);
+
+        // 저장한 글의 카테고리 반환
+        String category_name = postService.getCategoryName(saveId);
+
+        // 저장한 글이 존재하는 목록으로 리다이렉트
+        return "redirect:/community/post/"+category_name;
+    }
+
     /** 글 전체 조회 (카테고리별 & 페이징) **/
-    /* /{category_name}/post/page={pageNo}&orderby={orderCriteria} */
-    @GetMapping("/{category_name}/post")
+    /* /{category_name}/page={pageNo}&orderby={orderCriteria} */
+    @GetMapping("/{category_name}")
     public String readAllPost(@PathVariable(required = false) String category_name,
                           @RequestParam(required = false, defaultValue = "0", value = "page") int pageNo,
                           @RequestParam(required = false, defaultValue = "id", value = "orderby") String orderCriteria,
@@ -46,13 +63,17 @@ public class PostController {
         if(user != null){
             model.addAttribute("user", user);
         }
-        /*
-         기본적으로 보여줄 게시판 리스트는 `자유 게시판` 카테고리의 게시물들을 최신순으로 보여준다.
-         */
-        if(category_name.isEmpty()) category_name = "자유 게시판";
+
+        /* 기본적으로 보여줄 게시판 리스트는 `자유 게시판` 카테고리의 게시물들을 최신순으로 보여준다. */
+        if(category_name.isEmpty()) category_name = "chat"; // 자유게시판
+
+        /* 뷰에서 보여줄 카테고리명 & 영문명 반환 */
+        String viewName = categoryService.getPostViewName(category_name);
+        model.addAttribute("categoryViewName", viewName);
+        model.addAttribute("category_name", category_name);
 
         /* 카테고리 리스트 반환 */
-        List<CategoryDto.ResponseDto> categoryList = categoryService.findList();
+        List<PostCategoryDto.ResponseDto> categoryList = categoryService.findPostList();
         model.addAttribute("categoryList", categoryList);
 
         /** ========== 페이징 처리 ========== **/
@@ -71,45 +92,63 @@ public class PostController {
         model.addAttribute("pageNo", pageNo);
         model.addAttribute("pageVo", pageVo);
 
+
         return "community/post-all";
     }
 
-    /** 글 작성 페이지 반환 **/
-    @GetMapping("/write")
-    public String createForm(Model model,
-                             @AuthenticationPrincipal UserAdapter user){
-
-        /* 글 작성자가 카테고리를 선택할 수 있도록 함 */
-        List<CategoryDto.ResponseDto> categoryList = categoryService.findList();
-        model.addAttribute("categoryList", categoryList);
-
-        return "community/post-create";
-    }
+//    /** 글 작성 페이지 반환 **/
+//    @GetMapping("/write")
+//    public String createForm(Model model,
+//                             @AuthenticationPrincipal UserAdapter user){
+//
+//        /* 글 작성자가 카테고리를 선택할 수 있도록 함 */
+//        List<PostCategoryDto.ResponseDto> categoryList = categoryService.findPostList();
+//        model.addAttribute("categoryList", categoryList);
+//
+//        return "community/post-create";
+//    }
 
     /** 글 상세 조회 페이지 반환 **/
-    @GetMapping("/{category_name}/read/{post_id}")
-    public String readById(@PathVariable("post_id") Long post_id,
+    @GetMapping("/read/{category_name}/{post_id}")
+    public String readById(@PathVariable("category_name") String category_name,
+                           @PathVariable("post_id") Long post_id,
                            @AuthenticationPrincipal UserAdapter user,
                            Model model){
 
-        /* member_id 반환 */
-        Long member_id = user.getMember().getId();
 
         /* 게시물 DTO 반환 */
         PostDto.ResponseDto postResponseDto = postService.getById(post_id);
+        model.addAttribute("post", postResponseDto);
 
-        /* 로그인한 사용자와 게시물 작성자가 다르다면 조회수 업데이트 */
-        if(member_id != postResponseDto.getMember_id()){
-            postService.updateView(post_id);
+        boolean like = false; // 비로그인 유저라면 무조건 like = false;
+
+        if(user != null){
+            // 로그인한 사용자라면
+
+            /* member_id 반환 */
+            Long member_id = user.getMember().getId();
+            model.addAttribute("login_id", member_id);
+
+             /* 로그인한 사용자와 게시물 작성자가 다르다면 조회수 업데이트 */
+            if(member_id != postResponseDto.getMember_id()){
+                postService.updateView(post_id);
+            }
+
+            /* 현재 로그인한 유저가 이 게시물을 좋아요 했는지 안 했는지 여부 확인 */
+            like = postService.findLike(post_id, member_id);
         }
-        /* 댓글 DTO 반환 */
-        List<CommentDto.ResponseDto> commentListDto = commentService.findAllByPost(post_id);
-
-        /* 현재 로그인한 유저가 이 게시물을 좋아요 했는지 안 했는지 여부 확인 */
-        boolean like = postService.findLike(post_id, member_id);
 
         model.addAttribute("like", like);
-        model.addAttribute("post", postResponseDto);
+
+        /* 카테고리명 반환 */
+        model.addAttribute("category_name", category_name);
+
+        /* 카테고리 리스트 반환 */
+        List<PostCategoryDto.ResponseDto> categoryList = categoryService.findPostList();
+        model.addAttribute("categoryList", categoryList);
+
+        /* 댓글 DTO 반환 */
+        List<CommentDto.ResponseDto> commentListDto = commentService.findAllByPost(post_id);
         model.addAttribute("commentList", commentListDto);
 
         return "community/post-read";
@@ -117,9 +156,17 @@ public class PostController {
 
     /** 글 수정 페이지 반환 **/
     @GetMapping("/{category_name}/update/{post_id}")
-    public String update(@PathVariable("post_id") Long post_id,
-                  @AuthenticationPrincipal UserAdapter user,
-                  Model model){
+    public String update(@PathVariable("category_name") String category_name,
+                         @PathVariable("post_id") Long post_id,
+                      @AuthenticationPrincipal UserAdapter user,
+                    Model model){
+
+        /* 뷰에서 보여줄 카테고리명 반환 */
+        model.addAttribute("category_name", category_name);
+
+        /* 카테고리 리스트 반환 */
+        List<PostCategoryDto.ResponseDto> categoryList = categoryService.findPostList();
+        model.addAttribute("categoryList", categoryList);
 
         PostDto.ResponseDto post = postService.getById(post_id);
 
@@ -131,7 +178,7 @@ public class PostController {
 
     /** 글 검색 (카테고리별) 페이지 **/
     /* /{category_name}/search/keyword={keyword}&page={pageNo}&orderby={orderCriteria} */
-    @GetMapping("/{category_name}/search")
+    @GetMapping("/search/{category_name}")
     public String searchByCategory(@PathVariable("category_name") String category_name,
                                    @RequestParam("keyword") String keyword,
                                    @RequestParam(required = false, defaultValue = "0", value = "page") int pageNo,
@@ -140,16 +187,17 @@ public class PostController {
                                    @AuthenticationPrincipal UserAdapter user,
                                    Model model){
 
-        /*
-        기본적으로 보여줄 게시판 리스트는 `자유 게시판` 카테고리의 게시물들을 최신순으로 보여준다.
-         */
-        if(category_name.isEmpty()) category_name = "자유 게시판";
+        log.info("searchByCategory 진입");
 
         /* 카테고리 리스트 반환 */
-        List<CategoryDto.ResponseDto> categoryList = categoryService.findList();
+        List<PostCategoryDto.ResponseDto> categoryList = categoryService.findPostList();
         model.addAttribute("categoryList", categoryList);
 
-        /* ========== 페이징 처리 ========== */
+        /* 뷰에서 보여줄 카테고리명 반환 */
+        String viewName = categoryService.getPostViewName(category_name);
+        model.addAttribute("categoryViewName", viewName);
+
+        /** ========== 페이징 처리 ========== **/
 
         /*
         클라이언트 페이지에서 받은 pageNo 와 실제 접근 페이지는 다르다. Page 객체는 페이지가 0부터 시작
@@ -174,22 +222,17 @@ public class PostController {
     }
 
     /** 내가 작성한 글 페이지 조회 **/
-    /* /mypost/{category_name}/page={pageNo} */
-    @GetMapping("/mypost/{category_name}")
-    public String readMyPost(@PathVariable String category_name,
-                             @RequestParam(required = false, defaultValue = "0", value = "page") int pageNo,
+    /* /myPost/{category_name}/page={pageNo} */
+    @GetMapping("/myPost")
+    public String readMyPost(@RequestParam(required = false, defaultValue = "0", value = "page") int pageNo,
                              Pageable pageable,
                              @AuthenticationPrincipal UserAdapter user,
                              Model model){
-        /*
-        기본적으로 보여줄 게시판 리스트는 `자유 게시판` 카테고리의 게시물들을 최신순으로 보여준다.
-         */
-        if(category_name.isEmpty()) category_name = "자유 게시판";
+
 
         /* 카테고리 리스트 반환 */
-        List<CategoryDto.ResponseDto> categoryList = categoryService.findList();
+        List<PostCategoryDto.ResponseDto> categoryList = categoryService.findPostList();
         model.addAttribute("categoryList", categoryList);
-
 
         /* ========== 페이징 처리 ========== */
 
@@ -201,7 +244,7 @@ public class PostController {
 
         // 페이지 객체 생성
         Page<PostDto.ResponsePageDto> postResponsePageDto =
-                postService.getMyPostPageList(pageable, pageNo, user.getMemberDto().getId(), category_name);
+                postService.getMyPostPageList(pageable, pageNo, user.getMemberDto().getId());
 
         PageVo pageVo = postService.getPageInfo(postResponsePageDto, pageNo);
 
