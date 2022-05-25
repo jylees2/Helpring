0ede97cdbf9d09d145347fa7f15d25d1fefd3500
jylees2 <jyleedev@gmail.com>
2022-05-26 00:13:51 +0900

    api_key, api_secret final static final 상수 선언

--- src/main/java/com/jy/helpring/web/controller/admin/VerifyController.java
+++ src/main/java/com/jy/helpring/web/controller/admin/VerifyController.java
@@ -21,17 +21,20 @@ public class VerifyController {
 
     /** Iamport 결제 검증 컨트롤러 **/
     private final IamportClient iamportClient;
+    private static final String API_KEY = "4556500950256242";
+    private static final String API_SECRET = "f98fe01fd3806dd7443fd41074fc28edd1fae5244061228e33fc899e669ce106faece3f995cd6bf1";
 
     // 생성자를 통해 REST API 와 REST API secret 입력
     public VerifyController(){
-        this.iamportClient = new IamportClient("4556500950256242",
-                "f98fe01fd3806dd7443fd41074fc28edd1fae5244061228e33fc899e669ce106faece3f995cd6bf1");
+        this.iamportClient = new IamportClient(API_KEY, API_SECRET);
     }
 
     /** 프론트에서 받은 PG사 결괏값을 통해 아임포트 토큰 발행 **/
     @PostMapping("/{imp_uid}")
     public IamportResponse<Payment> paymentByImpUid(@PathVariable String imp_uid) throws IamportResponseException, IOException{
+
         log.info("paymentByImpUid 진입");
+
         return iamportClient.paymentByImpUid(imp_uid);
     }
 
